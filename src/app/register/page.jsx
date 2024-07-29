3560dddbe56599894957d31342264a8f486699cd
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-07-30 00:00:26 +0800

    fix: RegisterPage [layout] --fix the darkmode layout

---
+++ src/app/register/page.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useState } from "react";
+import { auth, firestore } from "../../lib/firebase";
+
+export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await firestore.collection("users").doc(user.uid).set({
+        uid: user.uid,
+        name,
+        email,
+        selectedCandidate: null,
+      });
+      alert("Registration successful!");
+    } catch (error) {
+      console.error("Error registering:", error);
+      alert("Error registering: " + error.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-primary w-full h-[100vh]">
+      <div className="flex flex-col border-white/30 shadow-sm/50 shadow-white p-8 border rounded-md md:w-1/2 lg:w-1/3">
+        <h4 className="scroll-m-20 text-white text-xl poppins-semibold">
+          Create an account
+        </h4>
+        <p className="[&:not(:first-child)]:mt-4 text-sm text-white/50 poppins-regular">
+          Enter your email below to create your account
+        </p>
+        <p className="[&:not(:first-child)]:mt-4 text-sm text-white poppins-regular">
+          Full Name
+        </p>
+        <input
+          placeholder="Eka Putra"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="border-white/30 bg-primary mt-2 p-2 border rounded-sm text-sm text-white placeholder:text-white/30 poppins-regular"
+        />
+        <p className="[&:not(:first-child)]:mt-4 text-sm text-white poppins-regular">
+          Email
+        </p>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="eka@gmail.com"
+          className="border-white/30 bg-primary mt-2 p-2 border rounded-sm text-sm text-white placeholder:text-white/30 poppins-regular"
+        />
+        <p className="[&:not(:first-child)]:mt-4 text-sm text-white poppins-regular">
+          Password
+        </p>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="*****"
+          className="border-white/30 bg-primary mt-2 p-2 border rounded-sm text-sm text-white placeholder:text-white/30 poppins-regular"
+        />
+        <button
+          className="bg-white mt-8 py-2 rounded-sm font-bold text-primary poppins-semibold"
+          onClick={handleRegister}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,16 @@
+// src/hooks/useAuth.js
+import { useEffect, useState } from "react";
+import { auth } from "../lib/firebase";
+
+export function useAuth() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return user;
+}
